e0b38571ffa7a21af13efd6f352962691ed939f5
Lukasz Maslanka <lukasz.maslanka@sw.ccac-ont.ca>
2015-02-03 13:11:34 -0500

    More functionality for the list

---
+++ Lambda.Lists.Tests/ListTests.cs
@@ -0,0 +1,115 @@
+﻿namespace Lambda.Lists.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    
+    [TestClass]
+    public class ListTests
+    {
+        [TestMethod]
+        public void NewListOfSizeZeroIsCreated()
+        {
+            const int ExpectedListSize = 0;
+            List<int> testList = new List<int>();
+
+            Assert.AreEqual(ExpectedListSize, testList.Length);
+        }
+
+        [TestMethod]
+        public void AddTwoItemsListSizeIsTwo()
+        {
+            const int ExpectedListSize = 2;
+            List<int> testList = new List<int>();
+
+            testList.Add(1001);
+            testList.Add(1002);
+
+            Assert.AreEqual(ExpectedListSize, testList.Length);
+        }
+
+        [TestMethod]
+        public void AddTwoItemsAccessTwoItemsFromList()
+        {
+            const int TestItemOne = 1001;
+            const int TestItemTwo = 1002;
+            List<int> testList = new List<int>();
+
+            testList.Add(TestItemOne);
+            testList.Add(TestItemTwo);
+
+            Assert.AreEqual(TestItemOne, testList[0]);
+            Assert.AreEqual(TestItemTwo, testList[1]);
+        }
+
+        [TestMethod]
+        public void AddTwoItemsRemoveFirstItemFromList()
+        {
+            const int TestItemOne = 1001;
+            const int TestItemTwo = 1002;
+
+            List<int> testList = new List<int>();
+
+            testList.Add(TestItemOne);
+            testList.Add(TestItemTwo);
+
+            int item = testList.RemoveAt(0);
+
+            Assert.AreEqual(TestItemOne, item);
+            Assert.AreEqual(1, testList.Length);
+        }
+
+        [TestMethod]
+        public void AddThreeItemsRemoveSecondItemFromList()
+        {
+            const int TestItemOne = 1001;
+            const int TestItemTwo = 1002;
+            const int TestItemThree = 1003;
+
+            List<int> testList = new List<int>();
+
+            testList.Add(TestItemOne);
+            testList.Add(TestItemTwo);
+            testList.Add(TestItemThree);
+
+            int item = testList.RemoveAt(1);
+
+            Assert.AreEqual(TestItemTwo, item);
+            Assert.AreEqual(2, testList.Length);
+        }
+
+        [TestMethod]
+        public void AddThreeItemsRemoveLastItemFromList()
+        {
+            const int TestItemOne = 1001;
+            const int TestItemTwo = 1002;
+            const int TestItemThree = 1003;
+
+            List<int> testList = new List<int>();
+
+            testList.Add(TestItemOne);
+            testList.Add(TestItemTwo);
+            testList.Add(TestItemThree);
+
+            int item = testList.RemoveAt(2);
+
+            Assert.AreEqual(TestItemThree, item);
+            Assert.AreEqual(2, testList.Length);
+        }
+
+        [TestMethod]
+        public void AddTwoItemsRemoveSpecificItemFromList()
+        {
+            const int TestItemOne = 1001;
+            const int TestItemTwo = 1002;
+
+            List<int> testList = new List<int>();
+
+            testList.Add(TestItemOne);
+            testList.Add(TestItemTwo);
+
+            int item = testList.Remove(TestItemOne);
+
+            Assert.AreEqual(TestItemOne, item);
+            Assert.AreEqual(1, testList.Length);
+        }
+    }
+}
---
+++ Lambda.Lists/List.cs
@@ -0,0 +1,82 @@
+﻿namespace Lambda.Lists
+{
+    using System;
+
+    public class List<T> where T : IComparable<T>, new()
+    {
+        public void Add(T item)
+        {
+            if (this.list.Length <= count)
+            {
+                var temp = new T[this.list.Length * 2];
+                Buffer.BlockCopy(this.list, 0, temp, 0, this.list.Length * sizeof(int));
+                this.list = temp;
+            }
+
+            this.list[count++] = item;
+        }
+
+        public T Remove(T item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            return this.RemoveAt(this.IndexOf(item));
+        }
+
+        public T RemoveAt(int index)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if ((uint)index >= (uint)count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            var item = this.list[index];
+            this.count--;
+
+            if (index < count)
+            {
+                int intSize = sizeof(int);
+                Buffer.BlockCopy(this.list, (index + 1) * intSize , this.list, index * intSize, (count - index) * intSize);
+            }
+            
+            return item;
+        }
+
+        public int IndexOf(T item)
+        {
+            return Array.IndexOf(this.list, item, 0, count);
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if ((uint)index >= (uint)this.list.Length)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                return this.list[index];
+            }
+        }
+
+        public List()
+        {
+            this.list = new T[1];
+        }
+
+        public int Length { get { return this.count; } }
+        public int Count { get { return this.count; } }
+        public int Capacity { get { return this.list.Length; } }
+
+        private T[] list;
+        private int count = 0;
+    }
+}
